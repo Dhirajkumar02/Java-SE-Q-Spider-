29468c7b487a33d4bf7b70712946b36ca43450f1
Dhiraj Kumar <dhirajkumar62413@gmail.com>
2024-07-11 20:33:16 +0530

    demo

--- Pattern_2.java
+++ Pattern_2.java
@@ -1,21 +1,14 @@
-class Pattern_2
-{
-  public static void main(String args[])
-    {
-      for(int row=1;row<=5;row++)
-       {
-         for(int col=1;col<=5;col++)
-           {
-             if(row==col)
-               {
-               System.out.print("*");
-               }
-             else
-               {
-                System.out.print("  ");
-               }
-           }
-          System.out.println();
-       }
+class Pattern_2 {
+  public static void main(String args[]) {
+    for (int row = 1; row <= 5; row++) {
+      for (int col = 1; col <= 5; col++) {
+        if (row == col) {
+          System.out.print("*");
+        } else {
+          System.out.print("  ");
+        }
+      }
+      System.out.println();
     }
+  }
 }
